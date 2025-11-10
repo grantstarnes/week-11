6ed465d9ca3a1307b2ff46fa111f2db373b4495a
Grant Starnes <gstarnes@iu.edu>
2025-11-10 23:52:10 +0000

    Set up functions for the exercise

--- apputil.py
+++ apputil.py
@@ -1,1 +1,15 @@
-# your code here
+# --------------- Importing Libraries ---------------
+import numpy as np
+from sklearn.cluster import KMeans
+
+# --------------- Exercise 1 ---------------
+def kmeans(X, k):
+    return None
+
+# --------------- Exercise 2 ---------------
+def kmeans_diamonds(n, k):
+    return None
+
+# --------------- Exercise 3 ---------------
+def kmeans_timer(n, k, n_iter = 5):
+    return None
