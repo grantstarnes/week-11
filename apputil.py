ba8095a9036c7176ed8c92bb99419dd6d8ae0d78
Grant Starnes <gstarnes@iu.edu>
2025-11-11 21:19:52 +0000

    Implemented exercise 2 code

--- apputil.py
+++ apputil.py
@@ -1,6 +1,7 @@
 # --------------- Importing Libraries ---------------
 import numpy as np
 from sklearn.cluster import KMeans
+import seaborn as sns
 
 # --------------- Exercise 1 ---------------
 def kmeans(X, k):
@@ -19,8 +20,22 @@ def kmeans(X, k):
     return centroids, labels
 
 # --------------- Exercise 2 ---------------
+
+# Loading the diamonds dataset from seaborn
+diamonds = sns.load_dataset("diamonds")
+
+# Only look at the numerical columns
+diamonds_num_columns = diamonds.select_dtypes(include = ['number'])
+
 def kmeans_diamonds(n, k):
-    return None
+    """
+    This function kmeans_diamonds takes in n and k, and uses the kmeans function from exercise 1 to 
+    cluster the first n rows of the numerical columns of the diamonds dataset into k clusters. It then 
+    returns the centroids and labels as a tuple (centroids, labels).
+    """
+    num_set = diamonds_num_columns.head(n).to_numpy()
+    centroids, labels = kmeans(num_set, k)
+    return centroids, labels
 
 # --------------- Exercise 3 ---------------
 def kmeans_timer(n, k, n_iter = 5):
